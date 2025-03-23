d990dc780f094467a2c2e146d4abba14d80b696d
solosage1 <bryan@usesolo.co>
2025-03-23 12:47:16 -0400

    Remove unnecessary IERC1155 and ERC1155 imports from FullRangePositions

--- src/FullRangePositions.sol
+++ src/FullRangePositions.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.26;
 
-import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Errors} from "./errors/Errors.sol";
 import {ERC6909Claims} from "v4-core/src/ERC6909Claims.sol";
 
