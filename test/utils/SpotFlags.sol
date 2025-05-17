d2cc4ca4e75f3e8355920d4fd55927fc4ad421bc
solosage1 <bryan@usesolo.co>
2025-05-16 22:09:06 -0400

    fix: RPC and hook flag validation issues - Fixed RPC URL formatting and updated hook flag dependencies

--- test/utils/SpotFlags.sol
+++ test/utils/SpotFlags.sol
@@ -8,18 +8,21 @@ import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 ///           ‣ afterInitialize
 ///           ‣ beforeSwap
 ///           ‣ afterSwap
+///           ‣ beforeSwapReturnDelta
 ///           ‣ afterSwapReturnDelta
 library SpotFlags {
     function required() internal pure returns (uint160) {
         return uint160(
             /* AFTER_INITIALIZE              */
-            Hooks.AFTER_INITIALIZE_FLAG
+            Hooks.AFTER_INITIALIZE_FLAG |
             /* BEFORE_SWAP                   */
-            | Hooks.BEFORE_SWAP_FLAG
+            Hooks.BEFORE_SWAP_FLAG |
             /* AFTER_SWAP                    */
-            | Hooks.AFTER_SWAP_FLAG
+            Hooks.AFTER_SWAP_FLAG |
+            /* BEFORE_SWAP_RETURN_DELTA      */
+            Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG |
             /* AFTER_SWAP_RETURN_DELTA       */
-            | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
+            Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
         );
     }
 }
