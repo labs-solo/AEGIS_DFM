6a0aa1e1fbb3cc44ef5d1a2319176eabe44ff783
Test User <test@example.com>
2025-10-06 13:32:53 -0400

    use solidity version 8.26

--- test/legacy/utils/DummyFullRangeHook.sol
+++ test/legacy/utils/DummyFullRangeHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
 import {TruncGeoOracleMulti} from "src/TruncGeoOracleMulti.sol";
 import {PoolId} from "v4-core/src/types/PoolId.sol";
